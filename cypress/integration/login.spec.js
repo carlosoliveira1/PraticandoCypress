46112aee836308aa9b71a18b80d495327afde5aa
carlosoliveira1 <carlosoliveiramauricio@gmail.com>
2021-08-22 22:09:24 -0300

    criando a conta no site

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -12,9 +12,34 @@ describe('Acesso', () => {
         cy.get('#SubmitCreate > span').click();
 
 
-        cy.wait(10000);
+        //cy.wait(10000);
 
+        // nesse passo o usuário foi direcionado para a tela de cadastro
+         
+        cy.get('#id_gender1').click();
+        cy.get('#customer_firstname').type('Carlos');
+        cy.get('#customer_lastname').type('Oliveira');
+        cy.get('#passwd').type('1234567');
+        cy.get('#days').select('5').should('have.value', '5')
+        cy.get('#months').select('7').should('have.value', '7')
+        cy.get('#years').select('1980').should('have.value', '1980')
+        cy.get('#optin').click()
+        cy.get('#company').type('Conductor')
+        cy.get('#address1').type('Rua do Carmo - 43 - 9º andar - 20011020  ')
+        cy.get('#address2').type('Rio de janeiro - 36')
+        cy.get('#city').type('Rio de janeiro')
+        cy.get('#id_state').select('Oklahoma').should('have.value', '36')
+        cy.get('#postcode').type('2002001')
+        cy.get('#other').type('Teste de campo de informação.')
+        cy.get('#phone').type('9865479898')
+        cy.get('#phone_mobile').type('9865479898')
+        cy.get('#alias').clear()// Foi a forma que encontrei para a apafar o texto que estava no campo
+        cy.get('#alias').type('carlosoloveiramauricio@gmail.com')
+
+       
 
 
     })
 })
+
+
